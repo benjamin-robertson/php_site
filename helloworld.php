4477245358596292d6d3ee79786419118aff3985
ben <benrobertson9876@gmail.com>
2024-03-07 17:56:02 +1100

    with get url

--- helloworld.php
+++ helloworld.php
@@ -1,2 +1,14 @@
 <?php
-echo '<h1>Hello World</h1>' . PHP_EOL;
+echo '<h1>Hello World</h1>';
+
+// $url = 'https://dog.ceo/api/breeds/image/random'
+$curl = curl_init('https://dog.ceo/api/breeds/image/random');
+
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_HEADER, 0);
+// curl_setopt($curl, CURLOPT_URL, $url);
+
+$result = curl_exec($curl;);
+
+echo $result;
+?>
