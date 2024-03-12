d1ae3cfc0f56aabf4da4c25fa56ce41d7560b668
ben <benrobertson9876@gmail.com>
2024-03-12 15:47:30 +1100

    acces last

--- arrays.php
+++ arrays.php
@@ -1,6 +1,5 @@
 <?php
 // An astronaut array assignment
-$astronaut = ['firstName' => 'Mark', 'Watney', 5 => 'Botanist'];
-
-// Access the last name value
-echo $astronaut[0] . '';
+$astronaut = ['firstName' => 'Mark', 6 => 'Watney', 5 => 'Botanist', 'STS395'];
+// Access the last element value
+echo $astronaut[6] . '';
