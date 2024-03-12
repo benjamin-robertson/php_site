8a8750a0bbb113283c36dff9edabd7ef674bbe8c
ben <benrobertson9876@gmail.com>
2024-03-12 15:52:29 +1100

    test

--- arrays.php
+++ arrays.php
@@ -1,12 +1,18 @@
 <?php
-// Build the crew
-$mission = [
-    'STS395' => [
-        ['firstName' => 'Mark', 'lastName' => 'Watney', 'specialty' => 'Botanist'],
-        ['firstName' => 'Melissa', 'lastName' => 'Lewis', 'specialty' => 'Commander'],
-        ['firstName' => 'Beth', 'lastName' => 'Johanssen', 'specialty' => 'Computer Specialist']
-        // How would you add a new crew member?
-    ]
+$config = [
+    'router' => [
+        'routes' => [
+            'market' => [
+                'type' => 'literal',
+                'options' => [
+                    'route' => '/market',
+                    'defaults' => [
+                        'controller' => 'IndexController',
+                        'action' => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
 ];
-// How would you access the Computer Specialist's first name?
-echo $mission['STS395'][2]['firstName'];
+echo $config['router']['routes']['market']['options']['defaults']['action'];
