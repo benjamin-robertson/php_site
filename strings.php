80718c93508de1ec1ebcc069945a3be360d80f44
ben <benrobertson9876@gmail.com>
2024-03-12 15:00:28 +1100

    strings

---
+++ strings.php
@@ -0,0 +1,6 @@
+<?php
+// Assign your name to a variable $name
+$name = 'Ben'; // replace "YOUR NAME" with your name
+echo 'Hello $name, welcome to class!';
+echo "Hello $name, welcome to class!";
+echo 'Hello ' . $name . ', welcome to class!';
