340093913601aaef7e26e853916c9ce10c74cde4
ben <benrobertson9876@gmail.com>
2024-03-12 14:48:26 +1100

    fix

--- constants.php
+++ constants.php
@@ -2,10 +2,10 @@
 // define a constant "FILE_NAME" that represents an arbitrary filename of your choice
 const FILE_NAME = '/tmp/blah';
 // Assign your first name to a variable $firstName
-$firstname = 'ben';
+$firstName = 'ben';
 // Write the contents of $firstName out to the file "FILE_NAME"
 file_put_contents(FILE_NAME, $firstName);
 // echo the OS family you are using
-echo $PHP_OS;
+echo PHP_OS;
 // echo the name of the current directory
 echo __dir__;
