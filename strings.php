5e3e634f6701fb791a241460ecc4d96de3cfb127
ben <benrobertson9876@gmail.com>
2024-03-12 15:01:17 +1100

    line break

--- strings.php
+++ strings.php
@@ -2,5 +2,7 @@
 // Assign your name to a variable $name
 $name = 'Ben'; // replace "YOUR NAME" with your name
 echo 'Hello $name, welcome to class!';
+echo '</br>';
 echo "Hello $name, welcome to class!";
+echo '</br>';
 echo 'Hello ' . $name . ', welcome to class!';
