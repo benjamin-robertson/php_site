5a475f6cda818fec649057a538364917998189ce
ben <benrobertson9876@gmail.com>
2024-03-12 14:42:29 +1100

    constants

---
+++ constants.php
@@ -0,0 +1,11 @@
+<?php
+// define a constant "FILE_NAME" that represents an arbitrary filename of your choice
+/* ??? */
+// Assign your first name to a variable $firstName
+/* ??? */
+// Write the contents of $firstName out to the file "FILE_NAME"
+file_put_contents(FILE_NAME, $firstName);
+// echo the OS family you are using
+echo ?;
+// echo the name of the current directory
+echo ?;
