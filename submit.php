64ee7570e146956df95060a5e3c2187237bf2bba
ben <benrobertson9876@gmail.com>
2024-03-13 14:24:04 +1100

    balh

--- submit.php
+++ submit.php
@@ -4,8 +4,11 @@ if ($_POST) {
     //  Filter inputs from user
 
     $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL)) ?? '';
+    $username = trim(strip_tags(filter_var($_POST['username'], FILTER_SANITIZE_STRING))) ?? '';
+    $password = trim($_POST['password']) ?? '';
     phpinfo(INFO_VARIABLES);
     echo "</br> $email";
+    echo "</br> $username";
 } else {
     echo 'Unhappy';
 }
