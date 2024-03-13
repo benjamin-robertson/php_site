aa7f6f13f700c310695c2ca2e4dc6eebcad8328b
ben <benrobertson9876@gmail.com>
2024-03-13 14:20:46 +1100

    fix

--- submit.php
+++ submit.php
@@ -3,7 +3,7 @@ if ($_POST) {
     $valid = 0;
     //  Filter inputs from user
 
-    $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL)) ?? '',
+    $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL)) ?? '';
     phpinfo(INFO_VARIABLES);
     echo "</br> $email";
 } else {
